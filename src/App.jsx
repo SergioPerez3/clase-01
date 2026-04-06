955bd4030d2f33005245fa9511e871f830b2bb01
Sergio Perez <Sergio_01_3@hotmail.com>
2026-04-06 20:22:29 +0200

    react

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  const name = "Sergio";
+  const LastName = "Perez"
+
+  return (
+    <> 
+      <h1>Hola {name} {LastName} </h1>
+      <p>Bienvenidos a todos </p>
+      <h2>Primera clase de React</h2>
+    </>
+  );
+}
+
+export default App
